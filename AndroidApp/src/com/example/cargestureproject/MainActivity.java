361bed1dba2616c783a0205cbae56ada6437e065
NoahPena <noahpena7@utexas.edu>
2015-05-27 18:08:16 -0500

    reverted back

--- AndroidApp/src/com/example/cargestureproject/MainActivity.java
+++ AndroidApp/src/com/example/cargestureproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cargestureproject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Method;
 import java.util.Set;
 import java.util.UUID;
 
@@ -11,10 +12,12 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends Activity 
 {
+	private static final String TAG = "MyActivity";
 
 	BluetoothAdapter mBluetoothAdapter;
 	BluetoothDevice mDevice;
@@ -37,20 +40,21 @@ public class MainActivity extends Activity
 		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 		
 		bluetoothOn();
-		try {
+		
+		/*try {
 			openBT();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		*/
 	}
 	
-	public void bluetoothOn()
+	public boolean bluetoothOn()
 	{
 		if(mBluetoothAdapter == null)
 		{
-			return;
+			return false;
 		}
 		else
 		{
@@ -61,26 +65,14 @@ public class MainActivity extends Activity
 			}
 		}
 		
-		 	Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-		    if(pairedDevices.size() > 0)
-		    {
-		        for(BluetoothDevice device : pairedDevices)
-		        {
-		            if(device.getName().equals("Nigga"))			//Please Change 
-		            {
-		                mDevice = device;
-		                break;
-		            }
-		        }
-		    }
-		    Toast.makeText(getApplicationContext(), "Bluetooth Device Found", Toast.LENGTH_LONG).show();
+		return true;
 	}
 	
-	public void bluetoothOff()
+	public boolean bluetoothOff()
 	{
 		if(mBluetoothAdapter == null)
 		{
-			return;
+			return false;
 		}
 		else
 		{
@@ -90,86 +82,7 @@ public class MainActivity extends Activity
 				Toast.makeText(getApplicationContext(), "Bluetooth Disabled", Toast.LENGTH_LONG).show();
 			}
 		}
-	}
-	
-	void openBT() throws IOException
-	{
-	    UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //Standard SerialPortService ID
-	    mSocket = mDevice.createRfcommSocketToServiceRecord(uuid);        
-	    mSocket.connect();
-	    //mOutputStream = mmSocket.getOutputStream();
-	    mInputStream = mSocket.getInputStream();
-
-	    beginListenForData();
-
-	    Toast.makeText(getApplicationContext(), "Bluetooth Opened", Toast.LENGTH_LONG).show();
-	    
-	}
-
-	void beginListenForData()
-	{
-	    final Handler handler = new Handler(); 
-	    final byte delimiter = 10; //This is the ASCII code for a newline character
-
-	    stopWorker = false;
-	    readBufferPosition = 0;
-	    readBuffer = new byte[1024];
-	    workerThread = new Thread(new Runnable()
-	    {
-	        public void run()
-	        {                
-	           while(!Thread.currentThread().isInterrupted() && !stopWorker)
-	           {
-	                try 
-	                {
-	                    int bytesAvailable = mInputStream.available();                        
-	                    if(bytesAvailable > 0)
-	                    {
-	                        byte[] packetBytes = new byte[bytesAvailable];
-	                        mInputStream.read(packetBytes);
-	                        for(int i=0;i<bytesAvailable;i++)
-	                        {
-	                            byte b = packetBytes[i];
-	                            if(b == delimiter)
-	                            {
-								     byte[] encodedBytes = new byte[readBufferPosition];
-								     System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
-								     final String data = new String(encodedBytes, "US-ASCII");
-								     readBufferPosition = 0;
-
-	                                handler.post(new Runnable()
-	                                {
-	                                    public void run()
-	                                    {
-	                                    	Toast.makeText(getApplicationContext(), "Data: " + data, Toast.LENGTH_LONG).show();
-	                                    }
-	                                });
-	                            }
-	                            else
-	                            {
-	                                readBuffer[readBufferPosition++] = b;
-	                            }
-	                        }
-	                    }
-	                } 
-	                catch (IOException ex) 
-	                {
-	                    stopWorker = true;
-	                }
-	           }
-	        }
-	    });
-
-	    workerThread.start();
-	}
-
-	void closeBT() throws IOException
-	{
-	    stopWorker = true;
-	    //mmOutputStream.close();
-	    mInputStream.close();
-	    mSocket.close();
-	    
-	    //myLabel.setText("Bluetooth Closed");
+		
+		return true;
 	}
 }
